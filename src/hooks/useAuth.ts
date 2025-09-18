6715430112417878cb850f161c74977ad38553ff
TsaqifRadhitya <tsaqifradhitya@gmail.com>
2025-09-18 22:29:34 +0700

    fix : remove getServerSideProps from application page

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -43,7 +43,7 @@ export const useAuth = () => {
                 setAuth(false);
             }
         };
-        if (!user) {
+        if (!user && !isAuth) {
             fetchUser();
         }
     }, []);
